5d7969fe885827447a67df8d185ee9dfe5517879
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-11 20:45:16 +0100

    test

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const video = document.querySelector("#video"); 
+const canvas = document.querySelector("#canvas"); 
+const context =canvas.getContext('2d');
+
+let model; 
+
+handTrack.startVideo(video).then(
+    status => {
+        if(status) {
+            navigator.getUserMedia(
+                {video: {}}, 
+                stream => {
+                    video.srcObject = stream; 
+                }, 
+                err => console.log(err)
+            );
+        }
+    }
+)
+
+handTrack.load().then(lmodel =>{
+    model = lmodel
+})
