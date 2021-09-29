88b97e6bcd4e97aa71c7fa4add2ec43f09533534
Gabriele Padovani <gabriele.padovani@gmail.com>
2021-09-29 18:53:09 +0200

    test2

--- src/js/article_content.js
+++ src/js/article_content.js
@@ -7,14 +7,15 @@ $(document).ready(function(){
 
     var converter = new showdown.Converter();
 
-    fetch(pageName).then(function(r){
-        var reader = new FileReader();
-        reader.readAsText(r.blob());
-
-        reader.onload(function(evt){
-            var fileString = evt.target.result;
-            $("#article_content").html(converter.makeHtml(fileString));
-        });
-    });
+    fetch(pageName).then((r) => r.blob.then((b)=>{
+            var reader = new FileReader();
+            reader.readAsText(b);
+    
+            reader.onload(function(evt){
+                var fileString = evt.target.result;
+                $("#article_content").html(converter.makeHtml(fileString));
+            });
+        })
+    );
 
 });
