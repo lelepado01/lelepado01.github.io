8eee17c3d9fa9240a8e8c4ed0ae624e3d46efaef
Gabriele Padovani <gabriele.padovani@gmail.com>
2021-09-30 13:25:57 +0200

    sidebar style

--- src/js/article_sidebar.js
+++ src/js/article_sidebar.js
@@ -6,22 +6,25 @@ var related_articles = [
 function createArticleSidebar(){
     var header_list = document.getElementsByTagName("h1"); 
 
-    var ls = $("<ul>")
+    var chapter_list = $("<ul>")
     for (let i = 0; i < header_list.length; i++) {
-        ls.append(
+        chapter_list.append(
             $("<li>").append($("<a>", {href:"#" + header_list[i].innerText}).text(header_list[i].innerText))
         )
     }
 
-    $("#article_sidebar").append($("<h2>").text("Chapters"))
-    $("#article_sidebar").append(ls);
+    $("#article_sidebar").append($("<h4>").text("Chapters"))
+    $("#article_sidebar").append(chapter_list);
 
     $("#article_sidebar").append($("<hr>")); 
-    $("#article_sidebar").append($("<h2>").text("Related Articles")); 
+    $("#article_sidebar").append($("<h4>").text("Related Articles")); 
     
+    var related_articles_list = $("<ul>");
     for (let i = 0; i < related_articles.length; i++) {
-        $("#article_sidebar").append(
+        related_articles_list.append(
             $("<li>").append($("<a>", {href:related_articles[i]}).text(related_articles[i]))
         )
     }
+
+    $("#article_sidebar").append(related_articles_list); 
 }
