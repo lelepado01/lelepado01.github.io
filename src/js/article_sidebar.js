8c1a7289dc1714a9637f86e81ddb611d5950db6d
Gabriele Padovani <gabriele.padovani@gmail.com>
2021-09-30 12:48:14 +0200

    test sidebar2

--- src/js/article_sidebar.js
+++ src/js/article_sidebar.js
@@ -1,5 +1,5 @@
 
 function createArticleSidebar(){
-    var header_list = $(":header"); 
-    console.log(header_list);  
+    var header_list = document.getElementsByTagName("h1"); 
+    console.log(header_list); 
 }
