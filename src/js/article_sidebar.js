e95eeed521ae35abd2de1874f863cfd3971580c6
Gabriele Padovani <gabriele.padovani@gmail.com>
2021-09-30 12:53:47 +0200

    sidebar linking and list

--- src/js/article_sidebar.js
+++ src/js/article_sidebar.js
@@ -1,5 +1,13 @@
 
 function createArticleSidebar(){
     var header_list = document.getElementsByTagName("h1"); 
-    console.log(header_list); 
+
+    var ls = $("<ul>")
+    for (let i = 0; i < header_list.length; i++) {
+        ls.append(
+            $("<li>").append($("<a>", {href:"#" + header_list[i].innerText}).text(header_list[i].innerText))
+        )
+    }
+
+    $("#article_sidebar").append(ls);
 }
