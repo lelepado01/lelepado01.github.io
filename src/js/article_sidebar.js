de7a5b07e90e999898fd4f02bccc2fa1933e39c1
Gabriele Padovani <gabriele.padovani@gmail.com>
2021-09-30 13:19:32 +0200

    sidebar layout

--- src/js/article_sidebar.js
+++ src/js/article_sidebar.js
@@ -1,4 +1,8 @@
 
+var related_articles = [
+    "Ciao"
+]; 
+
 function createArticleSidebar(){
     var header_list = document.getElementsByTagName("h1"); 
 
@@ -9,5 +13,15 @@ function createArticleSidebar(){
         )
     }
 
+    $("#article_sidebar").append($("<h2>").text("Chapters"))
     $("#article_sidebar").append(ls);
+
+    $("#article_sidebar").append($("<hr>")); 
+    $("#article_sidebar").append($("<h2>").text("Related Articles")); 
+    
+    for (let i = 0; i < related_articles.length; i++) {
+        $("#article_sidebar").append(
+            $("<li>").append($("<a>", {href:related_articles[i]}).text(related_articles[i]))
+        )
+    }
 }
