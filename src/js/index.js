6d0773a9961c72cae598673b2b89cb1ceeb55455
Gabriele Padovani <gabriele.padovani@gmail.com>
2021-10-30 11:43:48 +0200

    home  page  fix

--- src/js/index.js
+++ src/js/index.js
@@ -11,8 +11,8 @@ function createArticleBox(articlePath){
     return articleElement; 
 }
 
-async function getArticleDescription(articleName){
-    fetch("pages/" + articleName + ".md").then((r) => r.blob().then((b)=>{
+function fillArticleDescription(articleName){
+    fetch("pages" + articleName + ".md").then((r) => r.blob().then((b)=>{
         var reader = new FileReader();
         reader.readAsText(b);
 
@@ -21,7 +21,9 @@ async function getArticleDescription(articleName){
 
             for (let i = 0; i < fileLines.length; i++) {
                 if (fileLines[i].includes(DESCRIPTION_SECTION_TAG)){
-                    return $("<p>").text(fileLines[i].remove(DESCRIPTION_SECTION_TAG)); 
+                    $(ARTICLE_VIEWED).prepend(
+                        $("<p>").text(fileLines[i].replace(DESCRIPTION_SECTION_TAG, ""))
+                    );  
                 }                
             }
         };
@@ -32,15 +34,14 @@ async function getArticleDescription(articleName){
 function createArticlePreview(articlePath){
     $(ARTICLE_VIEWED).empty(); 
 
-    var articleDescription = getArticleDescription(articlePath);  
-    $(ARTICLE_VIEWED).append(articleDescription); 
-
     var articleBtn = $("<div>", {"class": "info_button"}).text("View full Article"); 
     articleBtn.click(function(){
         GoToPage(HTML_FOLDER + "article.html?page=" + getFileNameFromPath(articlePath));
     }); 
 
     $(ARTICLE_VIEWED).append(articleBtn); 
+
+    fillArticleDescription(articlePath);  
 }
 
 
@@ -50,11 +51,12 @@ function getArticleList(file_tree_structure, prefix){
     if (file_tree_structure.items){
         for (let i = 0; i < file_tree_structure.items.length; i++) {
 
+            var tmpprefix = prefix + "/";
             if (file_tree_structure.name) {
-                prefix = prefix + "/" + file_tree_structure.name; 
-            }
+                tmpprefix += file_tree_structure.name; 
+            } 
             
-            var subls = getArticleList(file_tree_structure.items[i], prefix); 
+            var subls = getArticleList(file_tree_structure.items[i], tmpprefix); 
             subls.forEach((elem)=>{ls.push(elem)}); 
         }
     } else {
